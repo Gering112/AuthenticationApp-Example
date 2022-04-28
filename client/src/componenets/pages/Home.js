bca26187e2d072c35b478c6011ed37961097b9c9
Gering Dong <GeringDong168@gmail.com>
2022-04-27 23:53:18 -0400

    Added a nav bar

---
+++ client/src/componenets/pages/Home.js
@@ -0,0 +1,28 @@
+const Home = () => {
+    const { oktaAuth, authState } = useOktaAuth();
+  
+    const login = async () => oktaAuth.signInWithRedirect();
+    const logout = async () => oktaAuth.signOut('/');
+  
+    if(!authState) {
+      return <div>Loading...</div>;
+    }
+  
+    if(!authState.isAuthenticated) {
+      return (
+        <div>
+          <p>Not Logged in yet</p>
+          <button onClick={login}>Login</button>
+        </div>
+      );
+    }
+  
+    return (
+      <div>
+        <p>Logged in!</p>
+        <button onClick={logout}>Logout</button>
+      </div>
+    );
+  };
+  
+  export default Home;
---
+++ client/src/componenets/pages/Staff.js
@@ -0,0 +1,1 @@
+<H1>Staff Page</H1>
